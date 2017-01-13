158fe644ffa4de6416f000be0abf6cb7ca3fc7c6
Ajinkya <ajinkyad@winsoftech.com>
2017-01-13 17:09:58 +0530

    First JUnit Test

--- Calculator/src/test/java/in/ajinkyadhote/calculator/CalculatorApplicationTests.java
+++ Calculator/src/test/java/in/ajinkyadhote/calculator/CalculatorApplicationTests.java
@@ -1,9 +1,13 @@
 package in.ajinkyadhote.calculator;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import in.ajinkyadhote.calculator.model.Calculator;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +15,13 @@ public class CalculatorApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+	
+	@Test
+	public void multiplicationByZeroShouldReturnZero() {
+		Calculator calc = new Calculator();
+		
+		assertEquals("2 * must be 0", 0, calc.multiply(10, 0));
 	}
 
 }
