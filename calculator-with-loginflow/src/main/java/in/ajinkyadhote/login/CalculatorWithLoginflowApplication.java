f20cac8833f733c7e393457037ec75ffa105b38c
Ajinkya <ajinkya.c.dhote@gmail.com>
2017-01-14 20:10:55 +0530

    Calculator with Login Flow

---
+++ calculator-with-loginflow/src/main/java/in/ajinkyadhote/login/CalculatorWithLoginflowApplication.java
@@ -0,0 +1,12 @@
+package in.ajinkyadhote.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorWithLoginflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorWithLoginflowApplication.class, args);
+	}
+}
