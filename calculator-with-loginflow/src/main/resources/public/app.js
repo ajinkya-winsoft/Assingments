71d2606a8c3eab34aef0f92c84cd50bc4d53f0e7
Ajinkya <ajinkya.c.dhote@gmail.com>
2017-01-15 22:43:20 +0530

    Added Calculator View And Improved Login Auth Flow

--- calculator-with-loginflow/src/main/resources/public/app.js
+++ calculator-with-loginflow/src/main/resources/public/app.js
@@ -1,6 +1,6 @@
-var routerApp = angular.module('routerApp', ['ui.router']);
+var myApp = angular.module('myApp', ['ui.router','toaster', 'ngAnimate']);
 
-routerApp.config(function($stateProvider, $urlRouterProvider) {
+myApp.config(function($stateProvider, $urlRouterProvider) {
     
     $urlRouterProvider.otherwise('/home');
     
@@ -24,16 +24,97 @@ routerApp.config(function($stateProvider, $urlRouterProvider) {
 	    
 });
 
-routerApp.controller("LoginController", function($scope, $http) {
+myApp.controller("myAppController", function($scope, $http, $state, toaster) {
+	
+	 $scope.pop = function(type, title, message){
+         toaster.pop(type, title, message);
+     };
+     
+    
 	$scope.login = function() {
 		$http.post("/login", "username=" + "user" +
 		        "&password=" + "password", {
 		            headers: {'Content-Type': 'application/x-www-form-urlencoded'}
 		        } ).then(function(data) {
-		            alert("login successful");
+		        	 $scope.pop("info","Welcome","Login Successful");
 		            localStorage.setItem("session", {});
+		            $scope.isLogin = true;
+		            $state.go("home");
 		        }, function(data) {
-		            alert("error logging in");
+		        	$scope.pop("error","Bad Credentials","Username or password incorrect");
+		            $scope.isLogin = false;
+		        });
+
+	}
+	
+	$scope.displayNo = function (id){
+		console.log(id)
+		var old_value = $scope.value;
+		//var displayField = document.getElementById("textfield");
+		
+		switch(id){
+			
+			case "clear" :
+				console.log($scope.value);
+				$scope.value = "";
+				break;
+			
+			case "divide" :
+				divideNumber(true);
+				break;
+				
+			case "plus" :
+				addNumber(true);
+				break;
+				
+			case "minus" :
+				subNumber(true);
+				break;
+				
+			case "multiply" :
+				multiplyNumber(true);
+				break;
+			
+			case "half" :
+				displayField.value = Number(old_value/2);
+				break;
+				
+			case "square" :
+				displayField.value = Number(old_value*old_value);
+				break;
+				
+		    case "delete" :
+				var text = displayField.value;
+				displayField.value = text.substring(0,(text.length)-1);
+				break;
+				
+			 case "modulo" :
+				modNumber(true);
+				break;
+			 
+			case "enter" :
+//				store_last_function();
+//			if(isOperatorPresent())
+//				process();
+//			else
+//				perform_last_operation();
+				$scope.process();
+		
+				break;
+				
+			default :
+				
+				$scope.value = $scope.value == undefined ? ""+id : $scope.value+""+id;
+		}
+	}
+	
+	$scope.process = function(){
+		$http.get("/calculator/eval/"+$scope.value)
+			.then(function(response) {
+		            $scope.value = response.data.result;
+		        }, function(data) {
+		          console.log("error");
+		          console.log(data)
 		        });
 
 	}
